0e8b949d31a30ab4bce8cf994d6e560a8b5a51c2
Georg Schmidt <development@gs-sec.de>
2024-01-21 16:39:26 +0100

    WLAN

---
+++ definitions/Wan.java
@@ -0,0 +1,22 @@
+package de.cryptosdk.objects.definitions;
+
+public class Wan {
+
+    // internal id
+    private String id;
+
+    // human friendly id
+    private String title;
+
+    // wlan ssid
+    private String ssid;
+
+    // wlan password for wpa 2/3
+    private String password;
+
+    // geolocation of the network
+    private String location;
+
+    // timestamp of this entry
+    private long lastUpdate;
+}
